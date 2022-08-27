d23e66c4c3a3d9eb0ac1957d1a7808e4708a1013
imjoymnht <mritunjoy.mahanta001@gmail.com>
2022-08-27 23:29:00 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import io from "socket.io-client";
+import axios from "axios";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Unstable_Grid2";
+
+const socket = io("http://localhost:5000");
 
 function App() {
+  const [count, setCount] = useState({
+    count1: 0,
+    count2: 0,
+    count3: 0,
+  });
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    socket.on("click", (payload) => {
+      setCount({ ...payload });
+    });
+  }, [count]);
+  console.log(isDisabled);
+  const handleClick1 = () => {
+    setCount({ ...count, count1: count["count1"] + 1 });
+    socket.emit("click", { ...count, count1: count.count1 + 1 });
+  };
+  const handleClick2 = () => {
+    setCount({ ...count, count2: count["count2"] + 1 });
+    socket.emit("click", { ...count, count2: count.count2 + 1 });
+  };
+  const handleClick3 = () => {
+    setCount({ ...count, count3: count["count3"] + 1 });
+    socket.emit("click", { ...count, count3: count.count3 + 1 });
+  };
+  const handleSubmit = async () => {
+    setIsDisabled(true);
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_API}/api/create-button-click`,
+      { count: Object.values(count) }
+    );
+    if (data.success) {
+      setIsDisabled(false);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" name="click">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid xs={3}>
+            <h2>{count.count1}</h2>
+            <Button variant="contained" onClick={handleClick1}>
+              Button 1
+            </Button>
+          </Grid>
+          <Grid xs={3}>
+            <h2>{count.count2}</h2>
+            <Button variant="contained" onClick={handleClick2}>
+              Button 2
+            </Button>
+          </Grid>
+          <Grid xs={3}>
+            <h2>{count.count3}</h2>
+            <Button variant="contained" onClick={handleClick3}>
+              Button 3
+            </Button>
+          </Grid>
+          <Grid xs={3}>
+            <br />
+            <br />
+            <br />
+            <Button
+              variant="contained"
+              onClick={handleSubmit}
+              disabled={isDisabled}
+            >
+              Update
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
